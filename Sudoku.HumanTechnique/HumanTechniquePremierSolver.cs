c4fc1b7aa8da11441f7a3c38d374655086208f23
Marie Girard <marie.girard@epfedu.fr>
2021-10-08 16:13:32 +0200

    initial commit

---
+++ Sudoku.HumanTechnique/HumanTechniquePremierSolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sudoku.Shared;
+
+namespace Sudoku.HumanTechnique
+{
+    public class HumanTechniquePremierSolver:ISolverSudoku
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }
+}
---
+++ Sudoku.HumanTechnique/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku.HumanTechnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
