1aa93cd00bb7533051e73d99f8e35a925c74bb39
Jean-Sylvain Boige <jsboige@gmail.com>
2021-10-08 14:41:38 +0200

    Solver ORTools vide

---
+++ Sudoku.ORToolsSolvers/ORToolsSimpleSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sudoku.Shared;
+
+
+namespace Sudoku.ORToolsSolvers
+{
+   public class ORToolsSimpleSolver:ISolverSudoku
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }
+}
