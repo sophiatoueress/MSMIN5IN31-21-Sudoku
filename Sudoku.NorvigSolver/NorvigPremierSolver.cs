552d191bc7b6dd4b758f8f93603d8efa064b4a71
Jean-Sylvain Boige <jsboige@gmail.com>
2021-10-19 12:02:43 +0200

    Correction Norvig premier solver

--- Sudoku.NorvigSolver/NorvigPremierSolver.cs
+++ Sudoku.NorvigSolver/NorvigPremierSolver.cs
@@ -1,6 +1,5 @@
 ﻿using Sudoku.Shared;
-using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Linq;
 
@@ -9,179 +8,24 @@ namespace Sudoku.NorvigSolver
     public  class NorvigPremierSolver : ISolverSudoku
     {
 
-        static string rows = "ABCDEFGHI";
-        static string cols = "123456789";
-        static string digits = "123456789";
-        static string[] squares = cross(rows, cols);
-        static Dictionary<string, IEnumerable<string>> peers;
-        static Dictionary<string, IGrouping<string, string[]>> units;
-
-        /*
-         * def cross(A, B):
-         *   return [a+b for a in A for b in B]
-         */
-        static string[] cross(string A, string B)
-        {
-            return (from a in A from b in B select "" + a + b).ToArray();
-
-        }
-
-
-        static string[][] zip(string[] A, string[] B)
-        {
-            var n = Math.Min(A.Length, B.Length);
-            string[][] sd = new string[n][];
-            for (var i = 0; i < n; i++)
-            {
-                sd[i] = new string[] { A[i].ToString(), B[i].ToString() };
-            }
-            return sd;
-        }
-
-
-
-        public static Dictionary<string, string> parse_grid(string grid)
-        {
-            var grid2 = from c in grid where "0.-123456789".Contains(c) select c;
-            var values = squares.ToDictionary(s => s, s => digits); //To start, every square can be any digit
-
-            foreach (var sd in zip(squares, (from s in grid select s.ToString()).ToArray()))
-            {
-                var s = sd[0];
-                var d = sd[1];
-
-                if (digits.Contains(d) && assign(values, s, d) == null)
-                {
-                    return null;
-                }
-            }
-            return values;
-        }
-
-        public static Dictionary<string, string> search(Dictionary<string, string> values)
-        {
-            if (values == null)
-            {
-                return null; // Failed earlier
-            }
-            if (all(from s in squares select values[s].Length == 1 ? "" : null))
-            {
-                return values; // Solved!
-            }
-
-            // Chose the unfilled square s with the fewest possibilities
-            var s2 = (from s in squares where values[s].Length > 1 orderby values[s].Length ascending select s).First();
-
-            return some(from d in values[s2]
-                        select search(assign(new Dictionary<string, string>(values), s2, d.ToString())));
-        }
-
-        static Dictionary<string, string> assign(Dictionary<string, string> values, string s, string d)
-        {
-            if (all(
-                    from d2 in values[s]
-                    where d2.ToString() != d
-                    select eliminate(values, s, d2.ToString())))
-            {
-                return values;
-            }
-            return null;
-        }
-
-
-        static Dictionary<string, string> eliminate(Dictionary<string, string> values, string s, string d)
-        {
-            if (!values[s].Contains(d))
-            {
-                return values;
-            }
-            values[s] = values[s].Replace(d, "");
-            if (values[s].Length == 0)
-            {
-                return null; //Contradiction: removed last value
-            }
-            else if (values[s].Length == 1)
-            {
-                //If there is only one value (d2) left in square, remove it from peers
-                var d2 = values[s];
-                if (!all(from s2 in peers[s] select eliminate(values, s2, d2)))
-                {
-                    return null;
-                }
-            }
-
-            //Now check the places where d appears in the units of s
-            foreach (var u in units[s])
-            {
-                var dplaces = from s2 in u where values[s2].Contains(d) select s2;
-                if (dplaces.Count() == 0)
-                {
-                    return null;
-                }
-                else if (dplaces.Count() == 1)
-                {
-                    // d can only be in one place in unit; assign it there
-                    if (assign(values, dplaces.First(), d) == null)
-                    {
-                        return null;
-                    }
-                }
-            }
-            return values;
-        }
-
-        static bool all<T>(IEnumerable<T> seq)
-        {
-            foreach (var e in seq)
-            {
-                if (e == null) return false;
-            }
-            return true;
-        }
-
-        static T some<T>(IEnumerable<T> seq)
-        {
-            foreach (var e in seq)
-            {
-                if (e != null) return e;
-            }
-            return default(T);
-        }
-
-        static string Center(this string s, int width)
-        {
-            var n = width - s.Length;
-            if (n <= 0) return s;
-            var half = n / 2;
-
-            if (n % 2 > 0 && width % 2 > 0) half++;
-
-            return new string(' ', half) + s + new String(' ', n - half);
-        }
-
-        /*
-        static Dictionary<string, string> print_board(Dictionary<string, string> values)
-        {
-            if (values == null) return null;
-
-            var width = 1 + (from s in squares select values[s].Length).Max();
-            var line = "\n" + String.Join("+", Enumerable.Repeat(new String('-', width * 3), 3).ToArray());
-
-            foreach (var r in rows)
-            {
-                Console.WriteLine(String.Join("",
-                    (from c in cols
-                     select values["" + r + c].Center(width) + ("36".Contains(c) ? "|" : "")).ToArray())
-                        + ("CF".Contains(r) ? line : ""));
-            }
-
-            Console.WriteLine();
-            return values;
-        }  */
+        
 
         public SudokuGrid Solve(SudokuGrid sorel)
         {
-            return search(parse_grid(sorel));
+            var stringSudoku = sorel.Cells.Aggregate("",
+                (strRows, row) => strRows + row.Aggregate("",
+                    (strCells, cell) => strCells + cell.ToString(CultureInfo.InvariantCulture)));
+            var dictSudoku = LinqSudokuSolver.parse_grid(stringSudoku);
+            var dictSolution = LinqSudokuSolver.search(dictSudoku);
+            var toReturn = new SudokuGrid();
+            foreach (var strCellPair in dictSolution)
+            {
+                var rowIndex = strCellPair.Key[0] - 'A';
+                var colIndex = strCellPair.Key[1] - '1';
+                toReturn.Cells[rowIndex][colIndex] = int.Parse(strCellPair.Value, CultureInfo.InvariantCulture);
+            }
+
+            return toReturn;
         }
 
     }
