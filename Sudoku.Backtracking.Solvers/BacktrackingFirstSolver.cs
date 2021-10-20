3ca7e496da072db0c176f63745cf95a764f1aaca
YorickScheidt <yorick.scheidt@epfedu.fr>
2021-10-21 00:54:41 +0200

    Solver fix

--- Sudoku.Backtracking.Solvers/BacktrackingFirstSolver.cs
+++ Sudoku.Backtracking.Solvers/BacktrackingFirstSolver.cs
@@ -16,13 +16,19 @@ namespace Sudoku.Backtracking.Solvers
 
         public bool Backtrack(SudokuGrid s,int x, int y)
         {
-            if (x==8 && y ==8)
+            if (x == 8 && y == 8)
             {
+                int[] possibilie = s.GetPossibilities(x, y);
+                s.Cells[x][y] = possibilie[0];
                 return true;
             }
             if (x > 8)
             {
                 return Backtrack(s, 0, y + 1);
+            }
+            if (s.Cells[x][y] != 0)
+            {
+               return Backtrack(s, x+1, y);
             }
             int[] possibilies = s.GetPossibilities(x, y);
             foreach (int i in possibilies)
@@ -31,6 +37,10 @@ namespace Sudoku.Backtracking.Solvers
                 if (Backtrack(s,x+1,y))
                 {
                     return true;
+                }
+                else
+                {
+                    s.Cells[x][y] = 0;
                 }
             }
             return false;
