e22e46684c1a5864f050dce77bf1b4be52a73486
CamilleBaz <78864251+CamilleBaz@users.noreply.github.com>
2021-10-21 19:42:51 +0200

    ajout selection elite+random

--- Sudoku.GeneticSharpSolvers/GeneticSharpPremierSolver.cs
+++ Sudoku.GeneticSharpSolvers/GeneticSharpPremierSolver.cs
@@ -18,9 +18,11 @@ namespace Sudoku.GeneticSharpSolvers
         public GeneticSharpSlowSolver()
         {
             PopulationSize = 5000;
-        }
-
+            crossover = new UniformCrossover();
+            mutation= new UniformMutation();
     }
+
+}
 
     public class GeneticSharpPremierSolver : ISolverSudoku
     {
@@ -28,7 +30,7 @@ namespace Sudoku.GeneticSharpSolvers
         private const double fitnessThreshold = 0;
         
         //Plutôt que de jouer sur le nombre de générations, j'utilise la notion de stagnation: une fois que la population s'est effondrée sur un minimum local, la fitness ne progresse plus, quelque soit le nombre de générations auquel cela se produit
-        private const int fitnessStagnationNb  = 20;
+        private const int fitnessStagnationNb  = 100;
 
 
         // J'ai passé les paramètres suivants sous forme de propriétés afin de pouvoir facilement créer des solvers alternatifs pour trouver le bon paramétrage
@@ -36,6 +38,13 @@ namespace Sudoku.GeneticSharpSolvers
         public int PopulationSize { get; set; } = 500;
         public float CrossoverProbability { get; set; } = 0.75f;
         public float MutationProbability { get; set; } = 0.2f;
+
+        // rend public les objets qui caractérisent AG
+        // instancier 
+        public SelectionBase selection { get; set; }= new MySelection();
+        public CrossoverBase crossover { get; set; }= new UniformCrossover() ;
+        public MutationBase mutation { get; set; }= new UniformMutation();
+
 
         public SudokuGrid Solve(SudokuGrid s)
         {
@@ -50,13 +59,20 @@ namespace Sudoku.GeneticSharpSolvers
             }
 
             IChromosome chromosome = new SudokuPermutationsChromosome(sudoku);
+           
             //var fitness = SudokuTestHelper.Eval(chromosome, sudoku, 50, 0, 10);
             var fitness = new SudokuFitness(sudoku);
-            var selection = new EliteSelection();
+
+            //var selection = new EliteSelection();
+            var selection = new MySelection();
+            selection.EveChromosome = new SudokuPermutationsChromosome(sudoku);
             var crossover = new UniformCrossover();
             var mutation = new UniformMutation();
 
+
+            //var mutation = new InsertionMutation();
             var population = new Population(PopulationSize, PopulationSize, chromosome);
+
             var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation)
             {
                 Termination = new OrTermination(new ITermination[]
@@ -64,18 +80,32 @@ namespace Sudoku.GeneticSharpSolvers
                     new FitnessThresholdTermination(fitnessThreshold),
                     new FitnessStagnationTermination(fitnessStagnationNb)
                 }),
+
+                //Termination = new FitnessThresholdTermination(fitnessThreshold),
                 MutationProbability = MutationProbability,
                 CrossoverProbability = CrossoverProbability,
+
                 // J'ai rajouté l'opérateur d'exécution en parallèle, qui permet de gagner un peu en performance en utilisant tous les cores de vos cpus
-                OperatorsStrategy = new TplOperatorsStrategy()
+
+               OperatorsStrategy = new TplOperatorsStrategy()
+               
+
             };
 
             ga.Start();
 
             var bestIndividual = ((ISudokuChromosome)ga.Population.BestChromosome);
-            var solution = bestIndividual.GetSudokus().First();
-            //return solutions.Max(solutionSudoku => fitness.Evaluate(solutionSudoku));
 
+            var solution = bestIndividual.GetSudokus().First();
+
+
+  
+
+            // faire une eval de la solution avec fitness (si fitness 0 = fini) de bestindividul
+            // si =! 0 je prends les 250 pop meilleurs et je remets 250 random 
+            // refaire tourner l'algo
+
+            //return solutions.Max(solutionSudoku => fitness.Evaluate(solutionSudoku));
 
             var toReturn = s.CloneSudoku();
 
@@ -86,8 +116,8 @@ namespace Sudoku.GeneticSharpSolvers
                     toReturn.Cells[ridx][cidx] = solution.GetCell(ridx, cidx);
                 }
             }
-            return toReturn;
 
+            return toReturn;
         }
 
 
