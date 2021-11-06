957b43551b93c944e43dbb7c99af9c176a3863b2
Marie Girard <marie.girard@epfedu.fr>
2021-11-06 12:44:59 +0100

    Added exploration

--- Sudoku.HumanTechnique/Core/HumanTechniqueExploration.cs
+++ Sudoku.HumanTechnique/Core/HumanTechniqueExploration.cs
@@ -4,12 +4,12 @@ using Sudoku.Shared;
 using Kermalis.SudokuSolver.Core;
 using System.Linq;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
-namespace Sudoku.HumanTechnique
+namespace Sudoku.HumanTechniqueExploration
 {
-    public class HumanTechniquePremierSolver:ISolverSudoku
+    public class HumanTechniqueExploration : ISolverSudoku
     {
-
         private class SolverTechnique
         {
             public Func<Puzzle, bool> Function { get; }
@@ -44,7 +44,7 @@ namespace Sudoku.HumanTechnique
 
         public SudokuGrid ConvertPuzzleToSudokuGrid(Puzzle p)
         {
-            return new SudokuGrid() { Cells = p.Columns.Select(r => r.Select(c => c.Value).ToArray()).ToArray()};
+            return new SudokuGrid() { Cells = p.Columns.Select(r => r.Select(c => c.Value).ToArray()).ToArray() };
         }
 
         public SudokuGrid Solve(SudokuGrid s)
@@ -52,7 +52,10 @@ namespace Sudoku.HumanTechnique
             Puzzle p = ConvertSudokuGridToPuzzle(s);
 
             bool solved; // If this is true after a segment, the puzzle is solved and we can break
-
+            bool full = false;
+            List<Cell> allCells = null;
+            bool deadEnd = false;
+            Stack<BackTrackingState> exploredCellValues = null;
             p.RefreshCandidates();
 
             do
@@ -84,31 +87,183 @@ namespace Sudoku.HumanTechnique
                 {
                     break;
                 }
-            } while (true);
 
+                full = p.Rows.All(row => row.All(c => c.Value != 0));
+
+                if (!full)
+                {
+                    // Les Sudokus les plus difficiles ne peuvent pas être résolus avec un stylo bille, c'est à dire en inférence pure.
+                    // Il va falloir lacher le stylo bille et prendre le crayon à papier et la gomme pour commencer une exploration fondée sur des hypothèses avec possible retour en arrière
+                    if (allCells == null)
+                    {
+                        allCells = p.Rows.SelectMany((row, rowIdx) => row).ToList();
+                        exploredCellValues = new Stack<BackTrackingState>();
+                    }
+                    //puzzle.RefreshCandidates();
+
+                    // Pour accélérer l'exploration et éviter de traverser la feuille en gommant trop souvent, on va utiliser les heuristiques des problèmes à satisfaction de contraintes
+                    // cf. les slides et le problème du "coffre de voiture" abordé en cours
+
+                    //heuristique MRV
+                    var minCandidates = allCells.Min(cell => cell.Candidates.Count > 0 ? cell.Candidates.Count : int.MaxValue);
+
+                    if (minCandidates != int.MaxValue)
+                    {
+                        // Utilisation de l'heuristique Deg: de celles qui ont le moins de candidats à égalité, on choisi celle la plus contraignante, celle qui a le plus de voisins (on pourrait faire mieux avec le nombre de candidats en commun avec ses voisins)
+                        var candidateCells = allCells.Where(cell => cell.Candidates.Count == minCandidates);
+                        //var degrees = candidateCells.Select(candidateCell => new {Cell = candidateCell, Degree = candidateCell.GetCellsVisible().Aggregate(0, (sum, neighbour) => sum + neighbour.Candidates.Count) });
+                        var degrees = candidateCells.Select(candidateCell => new { Cell = candidateCell, Degree = candidateCell.GetCellsVisible().Count(c => c.Value == 0) }).ToList();
+                        //var targetCell = list_cell.First(cell => cell.Candidates.Count == minCandidates);
+                        var maxDegree = degrees.Max(deg1 => deg1.Degree);
+                        var targetCell = degrees.First(deg => deg.Degree == maxDegree).Cell;
+
+                        //dernière exploration pour ne pas se mélanger les pinceaux
+
+                        BackTrackingState currentlyExploredCellValues;
+                        if (exploredCellValues.Count == 0 || !exploredCellValues.Peek().Cell.Equals(targetCell))
+                        {
+                            currentlyExploredCellValues = new BackTrackingState() { Board = p.GetBoard(), Cell = targetCell, ExploredValues = new List<int>() };
+                            exploredCellValues.Push(currentlyExploredCellValues);
+                        }
+                        else
+                        {
+                            currentlyExploredCellValues = exploredCellValues.Peek();
+                        }
+
+
+                        //utilisation de l'heuristique LCV: on choisi la valeur la moins contraignante pour les voisins
+                        var candidateValues = targetCell.Candidates.Where(i => !currentlyExploredCellValues.ExploredValues.Contains(i));
+                        var neighbourood = targetCell.GetCellsVisible();
+                        var valueConstraints = candidateValues.Select(v => new
+                        {
+                            Value = v,
+                            ContraintNb = neighbourood.Count(neighboor => neighboor.Candidates.Contains(v))
+                        }).ToList();
+                        var minContraints = valueConstraints.Min(vc => vc.ContraintNb);
+                        var exploredValue = valueConstraints.First(vc => vc.ContraintNb == minContraints).Value;
+                        currentlyExploredCellValues.ExploredValues.Add(exploredValue);
+                        targetCell.Set(exploredValue);
+                        //targetCell.Set(exploredValue, true);
+
+                    }
+                    else
+                    {
+                        //Plus de candidats possibles, on atteint un cul-de-sac
+                        if (p.IsValid())
+                        {
+                            solved = true;
+                        }
+                        else
+                        {
+                            deadEnd = true;
+                        }
+
+
+                        //deadEnd = true;
+                    }
+                }
+                else
+                {
+                    //If puzzle is full, it's either solved or a deadend
+                    if (p.IsValid())
+                    {
+                        solved = true;
+                    }
+                    else
+                    {
+                        deadEnd = true;
+                    }
+                }
+
+
+                if (deadEnd)
+                {
+                    //On se retrouve bloqué, il faut gommer et tenter d'autres hypothèses
+                    BackTrackingState currentlyExploredCellValues = exploredCellValues.Peek();
+                    //On annule la dernière assignation
+                    currentlyExploredCellValues.Backtrack(p);
+                    var targetCell = currentlyExploredCellValues.Cell;
+                    //targetCell.Set(0, true);
+                    while (targetCell.Candidates.All(i => currentlyExploredCellValues.ExploredValues.Contains(i)))
+                    {
+                        //on a testé toutes les valeurs possibles, On est à un cul de sac, il faut revenir en arrière
+                        exploredCellValues.Pop();
+                        if (exploredCellValues.Count == 0)
+                        {
+                            Debug.WriteLine("bug in the algorithm techniques humaines");
+                        }
+                        currentlyExploredCellValues = exploredCellValues.Peek();
+                        //On annule la dernière assignation
+                        currentlyExploredCellValues.Backtrack(p);
+                        targetCell = currentlyExploredCellValues.Cell;
+                        //targetCell.Set(0, true);
+                    }
+                    // D'autres valeurs sont possible pour la cellule courante, on les tente
+                    //utilisation de l'heuristique LCV
+                    var candidateValues = targetCell.Candidates.Where(i => !currentlyExploredCellValues.ExploredValues.Contains(i));
+                    var neighbourood = targetCell.GetCellsVisible();
+                    var valueConstraints = candidateValues.Select(v => new
+                    {
+                        Value = v,
+                        ContraintNb = neighbourood.Count(neighboor => neighboor.Candidates.Contains(v))
+                    }).ToList();
+                    var minContraints = valueConstraints.Min(vc => vc.ContraintNb);
+                    var exploredValue = valueConstraints.First(vc => vc.ContraintNb == minContraints).Value;
+                    currentlyExploredCellValues.ExploredValues.Add(exploredValue);
+                    targetCell.Set(exploredValue);
+                }
+
+
+            } while (!solved);
             return ConvertPuzzleToSudokuGrid(p);
+
+        }
+
+        public struct BackTrackingState
+        {
+            public Cell Cell { get; set; }
+
+            public List<int> ExploredValues { get; set; }
+
+            public int[][] Board { get; set; }
+
+
+            public void Backtrack(Puzzle objPuzzle)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (objPuzzle[i, j].Value != Board[i][j])
+                        {
+                            objPuzzle[i, j].Set(Board[i][j]);
+                        }
+                    }
+                }
+                objPuzzle.RefreshCandidates();
+            }
         }
 
         private static readonly SolverTechnique[] _techniques = new SolverTechnique[]
         {
-            new SolverTechnique(HiddenSingle, "Hidden single"),
-            new SolverTechnique(NakedPair, "https://hodoku.sourceforge.net/en/tech_naked.php#n2"),
-            new SolverTechnique(HiddenPair, "https://hodoku.sourceforge.net/en/tech_hidden.php#h2"),
-            new SolverTechnique(LockedCandidate, "https://hodoku.sourceforge.net/en/tech_intersections.php#lc1"),
-            new SolverTechnique(PointingTuple, "https://hodoku.sourceforge.net/en/tech_intersections.php#lc1"),
-            new SolverTechnique(NakedTriple, "https://hodoku.sourceforge.net/en/tech_naked.php#n3"),
-            new SolverTechnique(HiddenTriple, "https://hodoku.sourceforge.net/en/tech_hidden.php#h3"),
-            new SolverTechnique(XWing, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf2"),
-            new SolverTechnique(Swordfish, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf3"),
-            new SolverTechnique(YWing, "https://www.sudokuwiki.org/Y_Wing_Strategy"),
-            new SolverTechnique(XYZWing, "https://www.sudokuwiki.org/XYZ_Wing"),
-            new SolverTechnique(XYChain, "https://www.sudokuwiki.org/XY_Chains"),
-            new SolverTechnique(NakedQuadruple, "https://hodoku.sourceforge.net/en/tech_naked.php#n4"),
-            new SolverTechnique(HiddenQuadruple, "https://hodoku.sourceforge.net/en/tech_hidden.php#h4"),
-            new SolverTechnique(Jellyfish, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf4"),
-            new SolverTechnique(UniqueRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php"),
-            new SolverTechnique(HiddenRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php#hr"),
-            new SolverTechnique(AvoidableRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php#ar")
+    new SolverTechnique(HiddenSingle, "Hidden single"),
+    new SolverTechnique(NakedPair, "https://hodoku.sourceforge.net/en/tech_naked.php#n2"),
+    new SolverTechnique(HiddenPair, "https://hodoku.sourceforge.net/en/tech_hidden.php#h2"),
+    new SolverTechnique(LockedCandidate, "https://hodoku.sourceforge.net/en/tech_intersections.php#lc1"),
+    new SolverTechnique(PointingTuple, "https://hodoku.sourceforge.net/en/tech_intersections.php#lc1"),
+    new SolverTechnique(NakedTriple, "https://hodoku.sourceforge.net/en/tech_naked.php#n3"),
+    new SolverTechnique(HiddenTriple, "https://hodoku.sourceforge.net/en/tech_hidden.php#h3"),
+    new SolverTechnique(XWing, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf2"),
+    new SolverTechnique(Swordfish, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf3"),
+    new SolverTechnique(YWing, "https://www.sudokuwiki.org/Y_Wing_Strategy"),
+    new SolverTechnique(XYZWing, "https://www.sudokuwiki.org/XYZ_Wing"),
+    new SolverTechnique(XYChain, "https://www.sudokuwiki.org/XY_Chains"),
+    new SolverTechnique(NakedQuadruple, "https://hodoku.sourceforge.net/en/tech_naked.php#n4"),
+    new SolverTechnique(HiddenQuadruple, "https://hodoku.sourceforge.net/en/tech_hidden.php#h4"),
+    new SolverTechnique(Jellyfish, "https://hodoku.sourceforge.net/en/tech_fishb.php#bf4"),
+    new SolverTechnique(UniqueRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php"),
+    new SolverTechnique(HiddenRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php#hr"),
+    new SolverTechnique(AvoidableRectangle, "https://hodoku.sourceforge.net/en/tech_ur.php#ar")
         };
 
         public bool RunTechnique(Puzzle p)
@@ -175,8 +330,8 @@ namespace Sudoku.HumanTechnique
                                         }
                                         var pairs = new Cell[][]
                                         {
-                                            new Cell[] { cells[0], cells[3] },
-                                            new Cell[] { cells[1], cells[2] }
+                                    new Cell[] { cells[0], cells[3] },
+                                    new Cell[] { cells[1], cells[2] }
                                         };
                                         foreach (Cell[] pair in pairs)
                                         {
@@ -1106,3 +1261,5 @@ namespace Sudoku.HumanTechnique
         #endregion
     }
 }
+
+   
